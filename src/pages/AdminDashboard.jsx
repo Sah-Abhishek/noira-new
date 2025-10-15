1f0e9638a6e28dc722a20b70abdb4bfecbad89a7
sah-abhishek <abhisheksah3333@gmail.com>
2025-10-15 12:43:23 +0530

    removed three graphs

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -15,9 +15,9 @@ const AdminDashboard = () => {
         {/* Grid of 4 Graphs */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-6">
           <ActiveTherapistGraph title="Active Therapists" />
-          <ActiveTherapistGraph title="New Signups" />
-          <ActiveTherapistGraph title="Revenue Trend" />
-          <ActiveTherapistGraph title="Session Bookings" />
+          {/* <ActiveTherapistGraph title="New Signups" /> */}
+          {/* <ActiveTherapistGraph title="Revenue Trend" /> */}
+          {/* <ActiveTherapistGraph title="Session Bookings" /> */}
         </div>
 
       </div>
